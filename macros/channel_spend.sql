89614f5de3d38c22fa28b5d46602ceb48eb83ad0
zloff <zoe@poplindata.com>
2022-11-28 16:02:00 +1100

    Add example table to model comments, rename column in channel_counts

--- macros/channel_spend.sql
+++ macros/channel_spend.sql
@@ -1,3 +1,24 @@
+-- User supplied SQL script to extract total ad spend by channel, by replacing the SQL query in the default__channel_spend() macro.
+-- Required output schema:
+--  channel: STRING NOT NULL
+--  spend: FLOAT64 (Use the same monetary units as conversion revenue, and NULL if unknown.)
+--
+-- Note that all flags are passed into this template (e.g. conversion_window_start/end_date).
+
+-- Example (simplified) query:
+-- SELECT channel, 
+--        SUM(spend_usd) AS spend 
+-- FROM example_spend_table 
+-- GROUP BY 1
+
+-- Example table output for the user-supplied SQL:
+--   Channel     |  Spend
+--  ------------------------
+--   direct      |  1050
+--   paid_search |  10490 
+--   etc...
+
+
 {% macro channel_spend() %}
     {{ return(adapter.dispatch('channel_spend', 'snowplow_fractribution')()) }}
 {% endmacro %}
--- models/snowflake/snowplow_fractribution_channel_counts.sql
+++ models/snowflake/snowplow_fractribution_channel_counts.sql
@@ -3,8 +3,8 @@ SELECT
     campaign,
     source,
     medium,
-    COUNT(*) AS number_of_events
+    COUNT(*) AS number_of_sessions
 FROM
     {{ ref('snowplow_fractribution_sessions_by_customer_id') }}
 GROUP BY channel, campaign, source, medium
-ORDER BY channel, number_of_events DESC
+ORDER BY channel, number_of_sessions DESC
